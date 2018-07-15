67c684353891ddab9fa237a5aa6801f678b73c51
Thomas Newton <thomas_newton@mckinsey.com>
2018-07-14 21:59:17 -0400

    testing a change on CI

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to our Site</h1>
         </header>
         <p className="App-intro">{this.state.api}</p>
         <p className="App-intro">{this.state.rules}</p>
